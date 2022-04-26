fe7719370dcb02754eaae36fde5ed9af461af9ee
John Fieweger <john.fieweger98@gmail.com>
2022-04-26 18:55:09 +0200

    added animate lines for the winner

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ let fields = [];
 
 currentShape = 'cross';
 
+gameOver = false;
+
 function fillShape(id) {
-    if (!fields[id]) {
+    if (!fields[id] && !gameOver) { // Das Ausrufezeichen macht false zu true und umgekehrt
         if (currentShape == 'cross') {
             currentShape = 'circle';
             document.getElementById('player-1').classList.add('player-inactive');
@@ -35,27 +37,35 @@ function checkForWin() {
     let winner;
     if (fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {
         winner = fields[0];
+        document.getElementById('line-0').style.transform = 'scaleX(1)';
     }
     if (fields[3] == fields[4] && fields[4] == fields[5] && fields[3]) {
         winner = fields[3];
+        document.getElementById('line-1').style.transform = 'scaleX(1)';
     }
     if (fields[6] == fields[7] && fields[7] == fields[8] && fields[6]) {
         winner = fields[6];
+        document.getElementById('line-2').style.transform = 'scaleX(1)';
     }
     if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) {
         winner = fields[0];
+        document.getElementById('line-3').style.transform = 'rotate(90deg) scaleX(1)';
     }
     if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) {
         winner = fields[1];
+        document.getElementById('line-4').style.transform = 'rotate(90deg) scaleX(1)';
     }
     if (fields[2] == fields[4] && fields[4] == fields[7] && fields[2]) {
         winner = fields[2];
+        document.getElementById('line-5').style.transform = 'rotate(90deg) scaleX(1)';
     }
     if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {
         winner = fields[0];
+        document.getElementById('line-6').style.transform = 'rotate(45deg) scaleX(1.2)';
     }
     if (fields[2] == fields[4] && fields[4] == fields[6] && fields[2]) {
         winner = fields[2];
+        document.getElementById('line-7').style.transform = 'rotate(-45deg) scaleX(1.2)';
     }
     if (winner) {
         console.log('GEWONNEN', winner)
